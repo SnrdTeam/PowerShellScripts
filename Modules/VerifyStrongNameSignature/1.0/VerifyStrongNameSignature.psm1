7bff54b8889b9be9b5cca8b38c071b0b86b86452
Евгений Ажогин <azhogin@adeptik.com>
2017-03-15 15:39:13 +0400

    encoding

--- Modules/VerifyStrongNameSignature/1.0/VerifyStrongNameSignature.psm1
+++ Modules/VerifyStrongNameSignature/1.0/VerifyStrongNameSignature.psm1
@@ -6,8 +6,6 @@ function Test-StrongName {
         [Parameter(ValueFromPipeline=$true, Mandatory=$true)][string]$path
     )
 
-    [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
-
     $netFxTools = $env:NETFX_TOOLS
     if ($netFxTools -ne $null) {
         $snTool = "$netFxTools\x64\sn.exe"
@@ -24,6 +22,3 @@ function Test-StrongName {
         Write-Error "Assembly $path FAIL"        
     }
 }
-
-#$dir = "D:\Projects\SourceControl\AgentNetwork\Gateway\src\Gateway\bin\Debug\netcoreapp1.1"
-#Get-ChildItem -Depth 1 -File -Filter *.dll -Path $dir | %{Check-StrongName -path $_.FullName}
